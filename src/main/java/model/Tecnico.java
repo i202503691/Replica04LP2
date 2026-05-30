e3f00af01b440f66f37168cf89a8c228839f8bdc
msejuro312 <msejuro312@protonmail.com>
2026-05-29 23:59:57 -0500

    Completo

---
+++ src/main/java/model/Tecnico.java
@@ -0,0 +1,58 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table (name = "tbl_tecnico")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Tecnico {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column (name = "id_tecnico")
+	@EqualsAndHashCode.Include
+	private Integer idTecnico;
+	
+	@Column (name = "nombre")
+	private String nombre;
+	
+	@Column (name = "especialidad")
+	private String especialidad;
+	
+	public String getEspecialidadDescripcion() {
+		
+		switch (especialidad) {
+		case "S":
+			return "Software";
+		case "H":
+			return "Hardware";
+		case "R":
+			return "Redes";
+		case "B":
+			return "Base de datos";
+			
+			
+
+		default:
+			return "Desconocido";
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return nombre;
+	}
+
+}
